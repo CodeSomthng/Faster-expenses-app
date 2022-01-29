018c5ec469cd093b4832bd0d41c5dfaea8d56e77
Vladyslav <vladyslav.teleleiko@evowill.com>
2022-01-29 14:55:59 +0200

    configure views, create users_controlle.rb, user model, profile of user, posibility to edit user account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  # load_and_authorize_resource
+  before_action :authenticate_user!
+
+  # def cart
+  #   @cart = Cart.all.limit(10)
+  # end
+
+  def profile
+    @user = current_user
+  end
+
+  def edit; end
+end
